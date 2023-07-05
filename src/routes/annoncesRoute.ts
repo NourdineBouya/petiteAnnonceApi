444924e40d407ec65a64e37b2b66e761164a81f8
NourdineBouya <nourdinebouya@gmail.com>
2023-07-05 22:33:35 +0100

    crud steup

--- src/routes/annoncesRoute.ts
+++ src/routes/annoncesRoute.ts
@@ -1,13 +1,48 @@
 import { Router } from "express"
+import   {PrismaClient}  from '@prisma/client'
+ 
+const prisma = new PrismaClient()
 
  const router = Router()
 
-router.get('/' , (req : any, res : any)  => {
-    res.send('All Annonces')
+ 
+
+router.get('/' , async (req : any, res : any)  => {
+    const Annonces = await prisma.annonce.findMany()
+    res.send(Annonces)
+})
+router.get('/:id' , async (req , res )  => {
+    const {id} = req.params
+    const Annonce = await prisma.annonce.findUnique({where : {id : Number (id)}})
+    res.send(Annonce)
 })
 
-router.post('/' , (req : any, res : any)  => {
-    res.send('Create a Annonce')
+router.post('/' , async (req : any, res : any)  => {
+
+    const  {
+        title,
+        description,
+        price,
+        city,
+        salaray,
+        image
+    } = req.params
+
+    
+
+    const result  = await prisma.annonce.create({
+
+        
+        data : {
+        title,
+        description,
+        price,
+        city,
+        salaray,
+        image
+        } as any
+    }) 
+    res.send(result)
 })
 
 router.put('/:id' , (req : any, res : any)  => {
--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,19 +1,84 @@
 import { Router } from "express"
+import   {PrismaClient}  from '@prisma/client'
+ 
+const prisma = new PrismaClient()
 
  const router = Router()
 
-router.get('/' , (req : any, res : any)  => {
-    res.send('All Users')
+ 
+
+router.get('/' , async (req : any, res : any)  => {
+    const users = await prisma.user.findMany()
+    res.send(users)
+})
+router.get('/:id' , async (req , res )  => {
+    const {id} = req.params
+    const user = await prisma.user.findUnique({where : {id : Number (id)}})
+    res.send(user)
 })
 
-router.post('/' , (req : any, res : any)  => {
-    res.send('Create a Users')
+router.post('/' , async (req : any, res : any)  => {
+        res.setHeader('Content-Type', 'application/json');
+    const  {
+        name,
+        email,
+      username,
+      image,
+      bio,
+      phoneNumber,
+      passwordHash
+    } = req.params
+
+    
+        try {
+            const result  = await prisma.user.create({
+
+        
+                data : {
+                    name,
+                    email,
+                  username,
+                  image,
+                  bio,
+                  phoneNumber,
+                  passwordHash
+                } as any
+            }) 
+            res.json(result)
+        } catch (error) {
+            res.json({error})
+        }
+   
 })
 
-router.put('/:id' , (req : any, res : any)  => {
-    res.send(`Update a Users ${req.id}`)
+router.put('/:id' , async(req : any, res : any)  => {
+    const {id} = req.params
+    const {name , email , username , bio , phoneNumber , passwordHash} = req.body
+    try {
+        // update an existing record
+        const updateUser = await prisma.user.update({
+            where : {id : Number (id)},
+            data : {
+                name,
+                email,
+                username,
+                bio,
+                phoneNumber,
+                passwordHash
+            } as any
+        })
+        
+        res.json(updateUser)
+    } catch (error) {
+        res.status(400).json({error})
+    }
+    
 })
-router.put('/:id' , (req : any, res : any)  => {
-    res.send(`delete a Users ${req.id}`)
+router.delete('/:id' , async (req : any, res : any)  => {
+    const {id} = req.params     
+    await prisma.user.delete({ where:{ id :Number(id)}})
+     
 })
+
+
 export default router
